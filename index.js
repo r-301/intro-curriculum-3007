111d1048dcd280ea154bf09674758743c93b3131
r-301 <>
2020-04-18 18:07:57 +0900

    17で割り切れる数字を返す関数

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict';
+const numbers = new Map();
+
+
+function isMultipleOfSeventeen(n){
+    return n % 17 === 0;
+}
+
+Array.from(numbers)
+.filter(isMultipleOfSeventeen);
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
